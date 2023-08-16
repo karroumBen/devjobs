4210c870e1c5c3ab041eb1f2abf38ed678376df6
Abdelkarim Ben Karroum <garitomo@yahoo.fr>
2023-08-16 01:39:41 -0500

    new created routers added to index

--- server/index.js
+++ server/index.js
@@ -1,7 +1,15 @@
 const { app, startServer } = require('./server.js');
+const userRouter = require('./features/user/router.js');
+const jobPostRouter = require('./features/jobPosting/router.js');
+const jobApplicationRouter = require('./features/jobApplication/router.js');
 
 app.get('/', (req, res) => {
-  res.render('index');
+  console.log('got it')
+  res.send('test');
 })
+
+app.use('/api/users', userRouter);
+app.use('/api/jobposts', jobPostRouter);
+app.use('/api/job_applications', jobApplicationRouter);
 
 startServer();
